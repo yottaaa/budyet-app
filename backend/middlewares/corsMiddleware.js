79dab0d3168c5956b209416176448d7798225f68
Erick A. Bautista <yotterick7@gmail.com>
2024-12-29 15:08:05 +0800

    fix setting

--- backend/middlewares/corsMiddleware.js
+++ backend/middlewares/corsMiddleware.js
@@ -1,5 +1,5 @@
 export const corsMiddleware = (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL || "*");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept, X-Requested-With");
   res.header("Access-Control-Allow-Methods", "POST, GET, PUT, PATCH, DELETE");
   res.header("Access-Control-Allow-Credentials", "true"); // Enable cookies
